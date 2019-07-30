ddc9642bcaaceecb1c993f44f51beaf532b329f4
Mili Chang <michang@blizzard.com>
2019-07-30 16:14:51 +0800

    function complete.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,232 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app">
+        <b-container>
+            <b-row>
+                <draggable
+                        id="waiting"
+                        v-model="heroes"
+                        ghost-class="ghost"
+                        :sort="true"
+                        draggable=".item"
+                        :group="{ name: 'row' }"
+                    >
+                    <!-- <transition-group> -->
+                        <img v-for="(hero, heroindex) in heroes" :key="hero.id" class="item" :src="'dist/img/heroes/'+hero.img" :title="hero.name+' '+hero.name_en" width="60">
+                    <!-- </transition-group> -->
+                </draggable>
+            </b-row>
+
+            <b-row>
+                <div class="title">
+                    超恩惠
+                </div>
+                <draggable
+                        id="super_sync"
+                        v-model="super_sync"
+                        ghost-class="ghost"
+                        :sort="true"
+                        draggable=".item"
+                        :group="{ name: 'row' }"
+                    >
+                    <img v-for="(hero, heroindex) in super_sync" :key="hero.id" class="item" :src="'dist/img/heroes/'+hero.img" :title="hero.name+' '+hero.name_en" width="60">
+                </draggable>
+            </b-row>
+
+            <b-row>
+                <b-col>
+                    <div class="title">
+                        主力
+                    </div>
+                    <draggable
+                            id="main"
+                            v-model="main"
+                            ghost-class="ghost"
+                            :sort="true"
+                            draggable=".item"
+                            :group="{ name: 'row' }"
+                        >
+                        <img v-for="(hero, heroindex) in main" :key="hero.id" class="item" :src="'dist/img/heroes/'+hero.img" :title="hero.name+' '+hero.name_en" width="60">
+                    </draggable>
+                </b-col>
+                
+                <b-col>
+                    <div class="title">
+                        堪用
+                    </div>
+                    <draggable
+                            id="normal"
+                            v-model="normal"
+                            ghost-class="ghost"
+                            :sort="true"
+                            draggable=".item"
+                            :group="{ name: 'row' }"
+                        >
+                        <img v-for="(hero, heroindex) in normal" :key="hero.id" class="item" :src="'dist/img/heroes/'+hero.img" :title="hero.name+' '+hero.name_en" width="60">
+                    </draggable>
+                </b-col>
+
+                <b-col>
+                    <div class="title">
+                        廢柴
+                    </div>
+                    <draggable
+                            id="garbage"
+                            v-model="garbage"
+                            ghost-class="ghost"
+                            :sort="true"
+                            draggable=".item"
+                            :group="{ name: 'row' }"
+                        >
+                        <img v-for="(hero, heroindex) in garbage" :key="hero.id" class="item" :src="'dist/img/heroes/'+hero.img" :title="hero.name+' '+hero.name_en" width="60">
+                    </draggable>
+                </b-col>
+            </b-row>
+
+            <b-row>
+                <b-button block variant="primary" @click="save">儲存</b-button>
+                <b-button block variant="light" @click="clear">清除</b-button>
+            </b-row>
+        </b-container>
+        
+
+        
+
+        
+        <!-- <div id="garbage">
+            <ul>            
+                <li v-for="(garbageHeroId, index) in garbage" draggable="true">
+                    <span v-for="(hero, heroindex) in heroes" v-if="hero.id == garbageHeroId">
+                        <img :src="'dist/img/heroes/'+hero.img" :title="hero.name+' '+hero.name_en" width="60">
+                    </span>
+                </li>
+            </ul>
+        </div> -->
+    </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+    name: 'app',
+    data () {
+        return {
+            heroes: [
+                {
+                    id: 1,
+                    name: '光之戰士',
+                    name_en: 'WOL',
+                    img: 'wol.png',
+                },
+                {
+                    id: 2,
+                    name: '弗利奧尼爾',
+                    name_en: 'Firion',
+                    img: 'firion.png',
+                },
+            ],
+            super_sync: [],
+            main: [],
+            normal: [],
+            garbage: []
+        }
+    },
+    computed: {
+    },
+    mounted: function() {
+        if (localStorage.save) {
+            this.heroes = JSON.parse(localStorage.heroes);
+            this.super_sync = JSON.parse(localStorage.super_sync);
+            this.main = JSON.parse(localStorage.main);
+            this.normal = JSON.parse(localStorage.normal);
+            this.garbage = JSON.parse(localStorage.garbage);
+        }
+    },
+    methods: {
+        save: function(e) {
+            localStorage.heroes = JSON.stringify(this.heroes);
+            localStorage.super_sync = JSON.stringify(this.super_sync);
+            localStorage.main = JSON.stringify(this.main);
+            localStorage.normal = JSON.stringify(this.normal);
+            localStorage.garbage = JSON.stringify(this.garbage);
+            localStorage.save = true;
+            this.$swal({
+                text: '儲存成功！',
+                type: 'success',
+            });
+        },
+        clear: function(e) {
+            this.$swal({
+                text: '你確定要清除嗎？',
+                type: 'question',
+                showCancelButton: true,
+                confirmButtonText: '確定',
+                cancelButtonText: '取消',
+            }).then((result) => {
+                if(result.value) {
+                    localStorage.clear();
+                    window.location.reload();
+                    this.$swal({
+                        text: '清除成功!',
+                        type: 'success',
+                    });
+                } else {
+                   
+                }
+            })
+            
+        }
+    },
+    components: {
+        draggable,
+    },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap&subset=chinese-traditional');
+* {
+    font-family: 'Noto Sans TC', sans-serif;
 }
-
-h1, h2 {
-  font-weight: normal;
+#waiting,
+#super_sync,
+#main,
+#normal,
+#garbage{
+    min-height: 10px;
+    width: 100%;
+    background: #333;
+    padding: 10px;
+    img {
+        &:hover {
+            cursor: move;
+        }
+    }
+}
+.row {
+    padding-top: 20px;
+}
+.title {
+    width: 100%;
+    // margin-top: 20px;
+    margin-bottom: 10px;
+    font-size: 1.5em;
+    // font-weight: 400;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
-a {
-  color: #42b983;
+.ghost {
+    opacity: 0.5;
+    background: #c8ebfb;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(BootstrapVue)
+Vue.use(VueSweetalert2);
 
 new Vue({
   el: '#app',
